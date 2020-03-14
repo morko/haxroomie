6eadfe713138e65ea530a5da285d3d890a58dfda
Oskari Pöntinen <mail.morko@gmail.com>
2020-03-14 17:28:05 +0200

    Great commits continue. Developing install script.

--- scripts/install-haxroomie-cli-debian.sh
+++ scripts/install-haxroomie-cli-debian.sh
@@ -29,11 +29,23 @@ Usage: $0 [options]
 Options:
   -h|--help      Shows this help.
   -y|--yes       Answer yes to all questions (without interaction).
+
 EOF
 }
 
+function confirm_continue() {
+  while true; do
+    read -p "Are you sure you want to continue? [Y/n] " yn
+    case $yn in
+      [Yy]* ) break;;
+      [Nn]* ) exit;;
+      * ) break;;
+    esac
+  done
+}
+
 # Default values.
-QUIET=false
+NO_CONFIRM=false
 USERNAME="haxroomie"
 
 # Parse arguments.
@@ -46,7 +58,7 @@ while [[ $# -gt 0 ]]; do
     shift # past argument
     ;;
     -y|--yes)
-    QUIET=true
+    NO_CONFIRM=true
     shift # past argument
     ;;
     *)    # unknown option
@@ -55,43 +67,34 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-echo -e "\n\e[32mThis script will install haxroomie-cli and its dependencies.\e[0m"
-
-if [ "$QUIET" = false ]; then
-  while true; do
-    read -p "Are you sure you want to continue? [Y/n] " yn
-    case $yn in
-      [Yy]* ) break;;
-      [Nn]* ) exit;;
-      * ) break;;
-    esac
-  done
+if [ "$NO_CONFIRM" = false ]; then
+  echo -e "\n\e[32mThis script will install haxroomie-cli and its dependencies.\e[0m"
+  confirm_continue
 fi
 
-echo -e "\n\e[32mEnter user to run haxroomie as (user will be created if it does not exist).\e[0m"
-
-if [ "$QUIET" = false ]; then
-  read -p "User [default: $USERNAME]: " USERNAME
+if [ "$NO_CONFIRM" = false ]; then
+  echo -e "\n\e[32mEnter user to run haxroomie as (user will be created if it does not exist).\e[0m"
+  read -p "User [default: $USERNAME]: " CUSTOM_USERNAME
+  if ! [ -z "$CUSTOM_USERNAME" ]; then
+    USERNAME=$CUSTOM_USERNAME
+  fi
 fi
 
 egrep "^$USERNAME" /etc/passwd >/dev/null
 
 if [ $? -eq 0 ]; then
-  echo -e "\n\e[32mUser $USERNAME already exists!\e[0m"
-
-  if [ "$QUIET" = false ]; then
-    while true; do
-      read -p "Are you sure you want to continue? [Y/n] " yn
-      case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) exit;;
-        * ) break;;
-      esac
-    done
+  echo -e "\e[32mUser $USERNAME already exists!\e[0m"
+  if [ "$NO_CONFIRM" = false ]; then
+    confirm_continue
   fi
 else
   adduser $USERNAME
-  [ $? -eq 0 ] && echo "User $USERNAME has been added to system!" || echo "Failed to add a user!"
+  if [ $? -eq 0 ]; then
+    echo "\e[32mUser $USERNAME has been added to system!\e[0m"
+  else
+    echo -e "\e[31mERROR: Failed to add user!\e[0m"
+    exit 1
+  fi
 fi
 
 echo -e "\n\e[32mUpdating package cache...\e[0m"
@@ -103,22 +106,37 @@ if ! which curl > /dev/null; then
 fi
 
 echo -e "\n\e[32mInstalling dependencies for chromium browser...\e[0m"
-apt-get install -y gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
+apt-get install -y gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget screen
 
 if ! command -v nvm > /dev/null; then
   echo -e "\n\e[32mInstalling nvm (Node Version Manager)...\e[0m"
-  su -c "cd \$HOME && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash" $USERNAME
+  su - -c "cd \$HOME && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash" $USERNAME
+  if ! [ $? -eq 0 ]; then
+    echo -e "\e[31mERROR: Could not install nvm!\e[0m"
+    exit 1
+  fi
 fi
 
 if ! nvm ls $NODE_VERSION > /dev/null; then
   echo -e "\n\e[32mInstalling Node.js and setting version to ${NODE_VERSION}...\e[0m"
-  su -c "cd \$HOME && source .nvm/nvm.sh && nvm install $NODE_VERSION" $USERNAME
-  su -c "cd \$HOME && source .nvm/nvm.sh && nvm use $NODE_VERSION" $USERNAME
+  su - -c "cd \$HOME && source .nvm/nvm.sh && nvm install $NODE_VERSION" $USERNAME
+  if ! [ $? -eq 0 ]; then
+    echo -e "\e[31mERROR: Could not install Node.js!\e[0m"
+    exit 1
+  fi
+  su - -c "cd \$HOME && source .nvm/nvm.sh && nvm use $NODE_VERSION" $USERNAME
+  if ! [ $? -eq 0 ]; then
+    echo -e "\e[31mERROR: Could not set Node.js version!\e[0m"
+    exit 1
+  fi
 fi
 
 echo -e "\n\e[32mInstalling haxroomie-cli...\e[0m"
-su -c "cd \$HOME && source .nvm/nvm.sh && npm install -g haxroomie-cli" $USERNAME
+su - -c "cd \$HOME && source .nvm/nvm.sh && npm install -g haxroomie-cli" $USERNAME
+if ! [ $? -eq 0 ]; then
+  echo -e "\e[31mERROR: Could not install haxroomie-cli!\e[0m"
+  exit 1
+fi
 
 echo -e "\n\e[32mHaxroomie-cli installed!\e[0m"
-echo -e "\nSee https://github.com/morko/haxroomie/#quick-start for usage instructions."
-echo -e ""
+echo -e "See https://github.com/morko/haxroomie/packages/haxroomie-cli#quick-start for usage instructions.\e[0m"
